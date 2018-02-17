208b3bc81c1e419ed72cdfe2d85fd679dde2ac18
Natalia Jastrzębska <nataliak@10g.pl>
2018-02-17 14:55:31 +0100

    added api key

--- app/src/main/java/eu/napcode/popmovies/MainActivity.java
+++ app/src/main/java/eu/napcode/popmovies/MainActivity.java
@@ -2,6 +2,7 @@ package eu.napcode.popmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Natalia", "api key " + BuildConfig.TMDB_API_KEY);
     }
 }
