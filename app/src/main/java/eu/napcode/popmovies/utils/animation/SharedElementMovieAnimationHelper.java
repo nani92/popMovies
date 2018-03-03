c417fa111d8e5f809b1ca50f0a4795e1e47129a0
Natalia Jastrzębska <nataliak@10g.pl>
2018-03-03 12:20:07 +0100

    title animation

--- app/src/main/java/eu/napcode/popmovies/utils/animation/SharedElementMovieAnimationHelper.java
+++ app/src/main/java/eu/napcode/popmovies/utils/animation/SharedElementMovieAnimationHelper.java
@@ -13,7 +13,8 @@ public class SharedElementMovieAnimationHelper {
     public static Bundle getBundle(Activity activity, View view) {
         //TODO animate title
         Bundle bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
-                new Pair<View, String>(view.findViewById(R.id.posterImageView), activity.getString(R.string.poster_transition)))
+                new Pair<View, String>(view.findViewById(R.id.posterImageView), activity.getString(R.string.poster_transition)),
+                new Pair<View, String>(view.findViewById(R.id.titleTextView), activity.getString(R.string.title_transition)))
                 .toBundle();
 
         return bundle;
