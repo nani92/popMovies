c9d5a2ea658e03970c01f15875067cc53388bb2f
Natalia Jastrzębska <nataliak@10g.pl>
2018-03-12 17:24:16 +0100

    started new test

---
+++ app/src/test/java/eu/napcode/popmovies/ui/reviews/ReviewsPresenterTest.java
@@ -0,0 +1,10 @@
+package eu.napcode.popmovies.ui.reviews;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReviewsPresenterTest {
+
+
+}
