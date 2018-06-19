dadebe95950f5d453051cd52c447ae11e401f073
Natalia Jastrzębska <natalia@napcode.eu>
2018-06-19 13:43:53 +0200

    update

--- app/src/main/java/eu/napcode/popmovies/model/MoviesMapper.java
+++ app/src/main/java/eu/napcode/popmovies/model/MoviesMapper.java
@@ -7,22 +7,18 @@ import eu.napcode.popmovies.api.responsemodel.movie.ResponseMovie;
 public class MoviesMapper {
 
     public static Function<ResponseMovie, Movie> responseToMovie =
-            new Function<ResponseMovie, Movie>() {
+            responseMovie -> {
+                Movie movie = new Movie();
+                movie.setTitle(responseMovie.getTitle());
+                movie.setId(responseMovie.getId());
+                movie.setPosterPath(responseMovie.getPosterPath());
+                movie.setBackdropPath(responseMovie.getBackdropPath());
+                movie.setOriginalTitle(responseMovie.getOriginalTitle());
+                movie.setReleaseDate(responseMovie.getReleaseDate());
+                movie.setVoteAverage(String.valueOf(responseMovie.getVoteAverage()));
+                movie.setPlot(responseMovie.getOverview());
 
-                @Override
-                public Movie apply(ResponseMovie responseMovie) {
-                    Movie movie = new Movie();
-                    movie.setTitle(responseMovie.getTitle());
-                    movie.setId(responseMovie.getId());
-                    movie.setPosterPath(responseMovie.getPosterPath());
-                    movie.setBackdropPath(responseMovie.getBackdropPath());
-                    movie.setOriginalTitle(responseMovie.getOriginalTitle());
-                    movie.setReleaseDate(responseMovie.getReleaseDate());
-                    movie.setVoteAverage(String.valueOf(responseMovie.getVoteAverage()));
-                    movie.setPlot(responseMovie.getOverview());
-
-                    return movie;
-                }
+                return movie;
             };
 }
 
